dec191f3df9e90b3f2970845ac58199cff1fc139
pedro4morais <phsaiki@outlook.com>
2026-09-14 09:57:04 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,13 +3,16 @@
 
 using namespace std;
 
+// Constant value for pi
 const double PI = 3.14159;
 
+// Calculates the volume of a right circular cylinder
 double calculateVolume(double radius, double height)
 {
     return PI * radius * radius * height;
 }
 
+// Calculates the surface area of a right circular cylinder
 double calculateSurfaceArea(double radius, double height)
 {
     return 2 * PI * radius * radius + 2 * PI * radius * height;
@@ -17,14 +20,17 @@ double calculateSurfaceArea(double radius, double height)
 
 int main()
 {
+    // Hard-coded values used to test the program
     double radius = 5.0;
     double height = 10.0;
 
+    // Calculate volume and surface area
     double volume = calculateVolume(radius, height);
     double surfaceArea = calculateSurfaceArea(radius, height);
 
     cout << fixed << setprecision(2);
 
+    // Display the cylinder information
     cout << "Radius: " << radius << endl;
     cout << "Height: " << height << endl;
     cout << "Volume: " << volume << endl;
